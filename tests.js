08402b24faaddee267d09dd2db5e937ff053330a
genzoo <genzoo.kontakt@gmail.com>
2024-05-06 16:10:16 +0200

    simple web app with tests

---
+++ tests.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const axios = require('axios');
+
+axios.get('http://localhost:3000')
+  .then(response => {
+    assert.strictEqual(response.data, 'Hello World!');
+    console.log('Test passed successfully!');
+  })
+  .catch(error => {
+    console.error('Test failed:', error.message);
+  });
